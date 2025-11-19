e7dbb167c168db8a96333d5ebbeafd637ae9f65a
lukaskett <k.lukas15@seznam.cz>
2025-11-19 15:21:07 +0100

    fix edit path

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -48,7 +48,7 @@ const config: Config = {
           sidebarPath: "./sidebars.ts",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: "https://github.com/orienteerfeed/ofeed-docs/blob/main/docs",
+          editUrl: "https://github.com/orienteerfeed/ofeed-docs/blob/main/",
         },
         // blog: {
         //   showReadingTime: true,
